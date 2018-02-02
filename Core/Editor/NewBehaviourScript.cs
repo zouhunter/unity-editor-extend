788f84faed05aa3ef3bdfc51db874ecacf88f1c6
zouhangte <zouhantezbm@126.com>
2018-02-02 14:56:50 +0800

    初步构建脚本生成框架

---
+++ Core/Editor/NewBehaviourScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+using System.Collections;
+using System;
+using System.CodeDom;
+//using Microsoft.CSharp;
+//using System.CodeDom.Compiler;
+using System.IO;
+using System.Text;
+using System.Reflection;
+public class NewEditModeTest
+{
+    [Test]
+    public void Test()
+    {
+        //声明代码的部分
+        CodeCompileUnit compunit = new CodeCompileUnit();
+        CodeNamespace sample = new CodeNamespace("WorpClasses");
+        compunit.Namespaces.Add(sample);
+        //引用命名空间
+        sample.Imports.Add(new CodeNamespaceImport("System"));//导入System命名空间
+        sample.Imports.Add(new CodeNamespaceImport("System.Linq"));//导入System.Linq命名空间
+        //在命名空间下添加一个类
+        CodeTypeDeclaration wrapProxyClass = new CodeTypeDeclaration("类名");
+        //wrapProxyClass.BaseTypes.Add(baseType);// 如果需要的话 在这里声明继承关系 (基类 , 接口)
+        wrapProxyClass.CustomAttributes.Add(new CodeAttributeDeclaration("Serializable"));//添加一个Attribute到class上
+        sample.Types.Add(wrapProxyClass);//把这个类添加到命名空间 ,待会儿才会编译这个类
+        //为这个类添加一个无参构造函数  其实不添加也没事的, 只是做个demo而已
+        CodeConstructor constructor = new CodeConstructor();
+        constructor.Attributes = MemberAttributes.Public;
+        wrapProxyClass.Members.Add(constructor);
+        //为这个类添加一个方法   public override int 方法名(string str);
+        CodeMemberMethod method = new CodeMemberMethod();
+        method.Name = "方法名";
+        method.Attributes = MemberAttributes.Override | MemberAttributes.Public;//声明方法是公开 并且override的
+        method.Parameters.Add(new CodeParameterDeclarationExpression(typeof(string), "str")); //这个方法添加一个输入参数
+        method.ReturnType = new CodeTypeReference(typeof(int));//声明返回值的类型
+        method.Statements.Add(new CodeSnippetStatement(" return 1; ")); //方法体里面很简单 直接返回 一个1;
+
+        Microsoft.CSharp. CSharpCodeProvider cprovider = new Microsoft.CSharp.CSharpCodeProvider();
+        StringBuilder fileContent = new StringBuilder();
+        using (StringWriter sw = new StringWriter(fileContent))
+        {
+            cprovider.GenerateCodeFromCompileUnit(compunit, sw,new System.CodeDom.Compiler.CodeGeneratorOptions());//想把生成的代码保存为cs文件
+        }
+    }
+}
---
+++ Core/Editor/TempScriptWindow.cs
@@ -0,0 +1,377 @@
+﻿#region statement
+/*【author】        : zouhunter*/
+/*【time】          : 2018/2/2*/
+/*【description】   : 这是一个快速创建类模板的窗体扩展,帮助你减少重复劳动。*/
+/*
+        1.支持枚举、模型、结构和继承等类的模板。
+        2.支持快速创建通用型UI界面脚本
+        3.支持自定义模板类
+        4.自动生成作者、创建时间、描述等功能
+        5.支持工程同步（EditorPrefer）
+*/
+#endregion
+
+using System;
+using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using UnityEditorInternal;
+
+namespace EditorTools
+{
+    /// <summary>
+    /// 一个创建脚本模板的窗口
+    /// </summary>
+    public class TempScriptWindow : EditorWindow
+    {
+        [MenuItem("Tools/TempScriptWindow")]
+        static void Open()
+        {
+            var window = GetWindow<TempScriptWindow>();
+            window.wantsMouseMove = true;
+        }
+
+        [SerializeField]
+        private List<ScriptTemplate> templates;
+        private MonoScript script;
+        [SerializeField]
+        private bool isSetting;
+        [SerializeField]
+        private string authorName;
+        [SerializeField]
+        private string[] templateNames;
+        [SerializeField]
+        private int currentIndex;
+        [SerializeField]
+        private TempScriptHeader headerInfo;
+        private Vector2 scrollPos;
+        private ReorderableList detailList;
+        private void OnEnable()
+        {
+            InitEnviroment();
+            RegistTempates();
+        }
+
+        private void OnGUI()
+        {
+            DrawHead();
+            if (isSetting)
+            {
+                //绘制设置信息
+                DrawSettings();
+            }
+            else
+            {
+                currentIndex = GUILayout.Toolbar(currentIndex, templateNames);
+                using (var scroll = new EditorGUILayout.ScrollViewScope(scrollPos))
+                {
+                    scrollPos = scroll.scrollPosition;
+                    templates[currentIndex].OnGUI();
+                }
+                DrawFoot();
+            }
+
+        }
+
+        private void InitEnviroment()
+        {
+            if (headerInfo == null) headerInfo = new TempScriptHeader();
+            if (script == null) script = MonoScript.FromScriptableObject(this);
+            if(string.IsNullOrEmpty(authorName)) authorName = TempScriptHelper.GetAuthor();
+            if (string.IsNullOrEmpty(authorName))
+            {
+                isSetting = true;
+            }
+            if(detailList == null)
+            {
+                detailList = new ReorderableList(headerInfo.detailInfo, typeof(string),true,false,true,true);
+                detailList.onAddCallback += (x) => { headerInfo.detailInfo.Add(""); };
+                detailList.drawHeaderCallback = (x) => { EditorGUI.LabelField(x, "详细信息"); };
+                detailList.drawElementCallback += (x,y,z,w) => { headerInfo.detailInfo[y] = EditorGUI.TextField(x, headerInfo.detailInfo[y]); };
+            }
+        }
+
+        private void RegistTempates()
+        {
+            if(templates == null)
+            {
+                templates = new List<ScriptTemplate>();
+                templates.Add(new EnumScriptTemplate());
+                templates.Add(new StaticClassTemplate());
+                templates.Add(new DataModelTemplate());
+                templates.Add(new ExtendClassTemplate());
+                templates.Add(new StructTempate());
+                templates.Add(new UIPanelTempate());
+                templateNames = templates.ConvertAll<string>(x => x.Name).ToArray();
+            }
+        }
+
+        private void DrawHead()
+        {
+            using (var hor = new EditorGUILayout.HorizontalScope())
+            {
+                EditorGUILayout.ObjectField(script, typeof(MonoScript), false);
+                if (!isSetting && GUILayout.Button("setting", EditorStyles.miniButtonRight, GUILayout.Width(60)))
+                {
+                    isSetting = true;
+                }
+                else if (isSetting && GUILayout.Button("confer", EditorStyles.miniButtonRight, GUILayout.Width(60)) && !string.IsNullOrEmpty(authorName))
+                {
+                    isSetting = false;
+                }
+            }
+        }
+
+        private void DrawSettings()
+        {
+            using (var hor = new EditorGUILayout.HorizontalScope())
+            {
+                EditorGUI.BeginChangeCheck();
+                EditorGUILayout.SelectableLabel("作者:", EditorStyles.miniLabel, GUILayout.Width(60));
+                authorName = EditorGUILayout.TextField(authorName);
+                if (EditorGUI.EndChangeCheck())
+                {
+                    if (!string.IsNullOrEmpty(authorName))
+                    {
+                        TempScriptHelper.SaveAuthor(authorName);
+                    }
+                }
+            }
+        }
+        private void DrawFoot()
+        {
+            using (var horm = new EditorGUILayout.HorizontalScope())
+            {
+                EditorGUILayout.LabelField("命名", GUILayout.Width(60));
+                headerInfo.scriptName = EditorGUILayout.TextField(headerInfo.scriptName);
+                EditorGUILayout.LabelField("简介", GUILayout.Width(60));
+                headerInfo.description = EditorGUILayout.TextField(headerInfo.description);
+                if (GUILayout.Button("Create", EditorStyles.miniButtonRight, GUILayout.Width(60)))
+                {
+                    OnCreateButtonClicked();
+                }
+            }
+            detailList.DoLayoutList();
+        }
+
+        private void OnCreateButtonClicked()
+        {
+            if (ProjectWindowUtil.IsFolder(Selection.activeInstanceID))
+            {
+                var scriptStr = templates[currentIndex].Create(headerInfo);
+                if (!string.IsNullOrEmpty(scriptStr) && !string.IsNullOrEmpty(headerInfo.scriptName))
+                {
+                    var path = AssetDatabase.GetAssetPath(Selection.activeInstanceID);
+                    var scriptPath = string.Format("{0}/{1}.cs", path, headerInfo.scriptName);
+                    System.IO.File.WriteAllText(scriptPath, scriptStr, System.Text.Encoding.UTF8);
+                }
+            }
+            else
+            {
+                EditorUtility.DisplayDialog("路径不明", "请选中文件夹后重试", "确认");
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// 任何脚本的头
+    /// </summary>
+    [System.Serializable]
+    public class TempScriptHeader
+    {
+        public string author;
+        public string time;
+        public string description;
+        public List<string> detailInfo = new List<string>();
+        public string scriptName;
+    }
+
+    /// <summary>
+    /// 静态工具类
+    /// </summary>
+    public static class TempScriptHelper
+    {
+        private const string prefer_key = "temp_script_autor_name";
+
+        public static void SaveAuthor(string author)
+        {
+            EditorPrefs.SetString(prefer_key, author);
+        }
+
+        public static string GetAuthor()
+        {
+            return EditorPrefs.GetString(prefer_key);
+        }
+    }
+
+    /// <summary>
+    /// 代码创建模板的模板
+    /// </summary>
+    [System.Serializable]
+    public class ScriptTemplate
+    {
+        public virtual string Name { get { return null; } }
+        public virtual string Create(TempScriptHeader header) { return null; }
+        public virtual void OnGUI() { }
+    }
+
+    /// <summary>
+    /// 1.枚举类型脚本
+    /// </summary>
+    [Serializable]
+    public class EnumScriptTemplate : ScriptTemplate
+    {
+        public override string Name
+        {
+            get
+            {
+                return "Enum";
+            }
+        }
+        [SerializeField]
+        private List<string> types = new List<string>();
+        private ReorderableList reorderableList;
+
+        public EnumScriptTemplate()
+        {
+            Debug.Log("EnumScriptTemplate");
+            reorderableList = new ReorderableList(types, typeof(string));
+            reorderableList.onAddCallback += (x) => { types.Add(""); };
+            reorderableList.drawHeaderCallback += (x) => { EditorGUI.LabelField(x, "枚举列表"); };
+            reorderableList.drawElementCallback += (x, y, z, w) => { types[y] = EditorGUI.TextField(x, types[y]); };
+        }
+        public override string Create(TempScriptHeader header)
+        {
+            return "xxX";
+        }
+
+        public override void OnGUI()
+        {
+            reorderableList.DoLayoutList();
+        }
+    }
+
+    /// <summary>
+    /// 2.数据模拟类
+    /// </summary>
+    public class DataModelTemplate : ScriptTemplate
+    {
+        public override string Name
+        {
+            get
+            {
+                return "Model";
+            }
+        }
+
+        public override string Create(TempScriptHeader header)
+        {
+            return "";
+        }
+
+        public override void OnGUI()
+        {
+
+        }
+    }
+
+    /// <summary>
+    /// 3.静态类
+    /// </summary>
+    public class StaticClassTemplate : ScriptTemplate
+    {
+        public override string Name
+        {
+            get
+            {
+                return "Static";
+            }
+        }
+
+        public override string Create(TempScriptHeader header)
+        {
+            return "";
+        }
+
+        public override void OnGUI()
+        {
+
+        }
+    }
+
+    /// <summary>
+    /// 4.继承其他类的子类
+    /// </summary>
+    public class ExtendClassTemplate : ScriptTemplate
+    {
+        public override string Name
+        {
+            get
+            {
+                return "Extend";
+            }
+        }
+
+        public override string Create(TempScriptHeader header)
+        {
+            return "";
+        }
+
+        public override void OnGUI()
+        {
+
+        }
+    }
+
+    /// <summary>
+    /// 5.结构体模板
+    /// </summary>
+    public class StructTempate : ScriptTemplate
+    {
+        public override string Name
+        {
+            get
+            {
+                return "Struct";
+            }
+        }
+
+        public override string Create(TempScriptHeader header)
+        {
+            return "";
+        }
+
+        public override void OnGUI()
+        {
+
+        }
+    }
+
+    /// <summary>
+    /// UI模板
+    /// </summary>
+    public class UIPanelTempate : ScriptTemplate
+    {
+        public override string Name
+        {
+            get
+            {
+                return "UIPanel";
+            }
+        }
+
+        public override string Create(TempScriptHeader header)
+        {
+            return "";
+        }
+
+        public override void OnGUI()
+        {
+
+        }
+    }
+
+    ///下面可以自定义你的代码生成模板,并在窗体方法RegistTempates中注册
+    ///...
+}
